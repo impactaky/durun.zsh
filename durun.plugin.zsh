a39cfba74e880978ff4223b39b29ae72f758d772
impactaky <impactaky@users.noreply.github.com>
2024-05-23 12:04:29 +0900

    Ensure home directory owner is not root

--- durun.plugin.zsh
+++ durun.plugin.zsh
@@ -1,2 +1,3 @@
 DURUN_HOME=${DURUN_HOME:-$HOME/.durun/home}
+mkdir -p ${DURUN_HOME}
 alias durun='docker run --rm -it --net=host --group-add=sudo --user=$(id -u):$(id -g) --volume=/etc/group:/etc/group:ro --volume=/etc/passwd:/etc/passwd:ro --volume=/etc/shadow:/etc/shadow:ro -v $DURUN_HOME/$(pwd):$HOME -v $(pwd):$(pwd) --workdir=$(pwd) --group-add=video'
