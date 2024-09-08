78afaf59ad07dfe2c13db04baa1d0342ad2e5a89
impactaky <impactaky@users.noreply.github.com>
2024-09-08 23:31:58 +0900

    Fix home directory permission

--- durun.plugin.zsh
+++ durun.plugin.zsh
@@ -1,3 +1,20 @@
 DURUN_HOME=${DURUN_HOME:-$HOME/.durun/home}
-mkdir -p ${DURUN_HOME}
-alias durun='docker run --rm -it --net=host --group-add=sudo --user=$(id -u):$(id -g) --volume=/etc/group:/etc/group:ro --volume=/etc/passwd:/etc/passwd:ro --volume=/etc/shadow:/etc/shadow:ro -v $DURUN_HOME/$(pwd):$HOME -v $(pwd):$(pwd) --workdir=$(pwd) --group-add=video'
+
+function durun(){
+    mkdir -p ${DURUN_HOME}/$(pwd)
+    docker run --rm -it \
+        --net=host \
+        --group-add=sudo \
+        --group-add=video \
+        --user=$(id -u):$(id -g) \
+        --volume=/etc/group:/etc/group:ro \
+        --volume=/etc/passwd:/etc/passwd:ro \
+        --volume=/etc/shadow:/etc/shadow:ro \
+        -v $DURUN_HOME/$(pwd):$HOME \
+        -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
+        -v "$HOME/.gitconfig:$HOME/.gitconfig:ro" \
+        -v "$HOME/.config/gh:$HOME/.config/gh:ro" \
+        -v $(pwd):$(pwd) \
+        --workdir=$(pwd) \
+        $@
+}
